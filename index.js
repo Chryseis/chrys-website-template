7b7dff7175386b940e770d55dec3407d31940ba0
allen <294931812@qq.com>
2017-11-19 15:22:01 +0800

    first

---
+++ index.js
@@ -0,0 +1,32 @@
+/**
+ * Created by Administrator on 2017/11/19.
+ */
+const fs = require('fs');
+const path = require('path');
+
+console.log(path.resolve(__dirname, 'src'))
+
+let fileArr = fs.readdirSync(path.resolve(__dirname, 'src'));
+
+fileArr.forEach((item, i) => {
+    console.log(fs.statSync(path.resolve(__dirname, 'src', item)).isDirectory());
+})
+
+
+function getAllEntrys() {
+    let entrys = {};
+    let dir = path.resolve(__dirname, 'src');
+    let directoryArr = fs.readdirSync(dir);
+    directoryArr.forEach((item, i) => {
+        if (fs.statSync(path.join(dir, item)).isDirectory()) {
+            let fileArr = fs.readdirSync(path.join(dir, item));
+            fileArr.forEach((file, i) => {
+                if (!fs.statSync(path.join(dir, item, file)).isDirectory()) {
+                    entrys[item]
+                }
+            })
+        }
+    })
+}
+
+console.log(fileArr)
