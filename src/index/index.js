557efb7102cb36f71f6d0de017a99f0c6b3ac2cd
allen <294931812@qq.com>
2017-12-19 00:44:05 +0800

    website template

--- src/index/index.js
+++ src/index/index.js
@@ -4,10 +4,33 @@
 import './index.less';
 
 
-const a = () => {
-    console.log('haha')
-}
+$(function () {
+    $('#btn1').click(function () {
+        rotatePolygon(30);
+    })
 
-$('.header').click(() =>{
-    a();
-});
+    $('#btn2').click(function () {
+        rotatePolygon(60);
+    })
+
+    $('#btn3').click(function () {
+        rotatePolygon(90);
+    })
+
+    $('#btn4').click(function () {
+        rotatePolygon(120);
+    })
+
+    $('#btn5').click(function () {
+        rotatePolygon(150);
+    })
+
+    function rotatePolygon(deg) {
+        $('.wrapper').attr('style', `transform:rotate(${deg}deg)`);
+        $('.item').addClass('active');
+        $('.center-point').addClass('active');
+        $('.wrapper').on('transitionend', function () {
+            $(this).children('.item').removeClass('active')
+        })
+    }
+})
